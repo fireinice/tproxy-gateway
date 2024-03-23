8b41d4428998580fb559fc7b9b24acda144410cd
zigler zhang <zigler.zhang@gmail.com>
2024-03-24 03:20:00 +0800

    +FEAT: 增加dns转发

--- scripts/async_ipset.sh
+++ scripts/async_ipset.sh
@@ -3,6 +3,10 @@ DEST_DIR=$1
 ONAME=`echo $2 | md5sum | cut -d ' ' -f 1`
 outf="$DEST_DIR/$ONAME.tmp"
 wget -q $2 -O $outf
+if [ $? -ne 0 ]; then
+    echo "Failed to load ipset from $2"
+    exit 1
+fi
 sed -i "s/^/add ${3} /" $outf
 ipset restore < $outf
 rm $outf
--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -3,11 +3,14 @@
 ip rule add fwmark 1 table 100
 ip route add local 0.0.0.0/0 dev lo table 100
 
-# iptables -t nat -N clash_dns
-# iptables -t nat -A OUTPUT -p udp --dport 53 -j clash_dns
-# iptables -t nat -F clash_dns
-# iptables -t nat -A clash_dns -p udp --dport 53 -j REDIRECT --to-ports 7853
-
+if [ -n "$DNS_PORT" ]; then
+iptables -t nat -N clash_dns
+iptables -t nat -F clash_dns
+iptables -t nat -A clash_dns -m set --match-set bypass_private dst -j RETURN
+iptables -t nat -A clash_dns -m set --match-set bypass_dest dst -j RETURN
+iptables -t nat -A clash_dns -p udp -j REDIRECT --to-port $DNS_PORT
+iptables -t nat -I PREROUTING -p udp --dport 53 -j clash_dns
+fi
 # CREATE TABLE
 iptables -t mangle -N clash
 
@@ -18,8 +21,8 @@ iptables -t mangle -A clash -m set --match-set bypass_source src -j RETURN
 iptables -t mangle -A clash -m set --match-set bypass_mac_src src -j RETURN
 
 # FORWARD ALL
-iptables -t mangle -A clash -p udp -j TPROXY --on-port $PROXY_PORT --on-ip 0.0.0.0 --tproxy-mark 1
-iptables -t mangle -A clash -p tcp -j TPROXY --on-port $PROXY_PORT --on-ip 0.0.0.0 --tproxy-mark 1
+iptables -t mangle -A clash -p udp -j TPROXY --on-port $TPROXY_PORT --on-ip 0.0.0.0 --tproxy-mark 1
+iptables -t mangle -A clash -p tcp -j TPROXY --on-port $TPROXY_PORT --on-ip 0.0.0.0 --tproxy-mark 1
 
 # REDIRECT
 iptables -t mangle -A PREROUTING -j clash
