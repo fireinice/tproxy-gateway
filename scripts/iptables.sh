2bdcf371fbe17263c71a6de80cd52ae20d81c40b
zigler zhang <zigler.zhang@gmail.com>
2024-03-31 16:42:23 +0800

    =FIX: 去除dot的劫持

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -9,7 +9,6 @@ if [ -n "$DNS_PORT" ]; then
     iptables -t nat -A clash_dns -p udp -j REDIRECT --to-port $DNS_PORT
     iptables -t nat -A clash_dns -p tcp -j REDIRECT --to-port $DNS_PORT
     iptables -t nat -A PREROUTING -p udp --dport 53 -j clash_dns
-    iptables -t nat -A PREROUTING -p tcp --dport 853 -j clash_dns
 fi
 
 # CREATE TABLE
@@ -18,7 +17,6 @@ iptables -t mangle -N clash
 # RETURN LOCAL AND LANS
 # for later redirect
 iptables -t mangle -A clash -p udp --dport 53 -j RETURN
-iptables -t mangle -A clash -p tcp --dport 853 -j RETURN
 iptables -t mangle -A clash -m set --match-set bypass_private dst -j RETURN
 iptables -t mangle -A clash -m set --match-set bypass_dest dst -j RETURN
 iptables -t mangle -A clash -m set --match-set bypass_source src -j RETURN
