8296ed7da8ad8973387d8872fb2d17776b27d50c
zigler zhang <zigler.zhang@gmail.com>
2024-04-03 00:35:40 +0800

    +FEAT: 5分钟内尝试多次使从url加载的ipset可以加载

--- scripts/async_ipset.sh
+++ scripts/async_ipset.sh
@@ -2,13 +2,24 @@
 DEST_DIR=$1
 ONAME=`echo $2 | md5sum | cut -d ' ' -f 1`
 outf="$DEST_DIR/$ONAME.tmp"
-wget -q $2 -O $outf
-if [ $? -ne 0 ]; then
-    echo "Failed to update ipset from $2"
-    ipset restore < $outf
-    echo "Successfully loaded ipset from backup $outf"
-else
-    sed -i "s/^/add ${3} /" $outf
-    ipset restore < $outf
-    echo "Successfully loaded ipset from $2"
-fi
+counter=0
+while [ $counter -lt 10 ]
+do
+    wget -q $2 -O $outf
+    if [ $? -eq 0 ]; then
+	sed -i "s/^/add ${3} /" $outf
+	echo "Successfully update ipset from $2"
+	counter=10
+    else
+	echo "failed update ipset from $2, retrying.. ${counter} times"
+    fi
+    if [ $counter -eq 10 ]; then
+	ipset restore < $outf
+	echo "Successfully loaded ipset from $2"
+    elif [ $counter -ge 4 ]; then
+	ipset restore < $outf
+	echo "Successfully loaded ipset from backup"
+    fi
+    sleep 30
+    counter=$((counter + 1))
+done
