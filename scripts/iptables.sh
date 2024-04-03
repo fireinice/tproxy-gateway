50d15c23e0a3d45ff39faba91742fd430238a452
zigler zhang <zigler.zhang@gmail.com>
2024-04-04 01:29:06 +0800

    +FIX: 不再监听53端口的tcp协议

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -7,7 +7,6 @@ if [ -n "$DNS_PORT" ]; then
     iptables -t nat -N clash_dns
     iptables -t nat -A clash_dns -m set --match-set bypass_private dst -j RETURN
     iptables -t nat -A clash_dns -p udp -j REDIRECT --to-port $DNS_PORT
-    iptables -t nat -A clash_dns -p tcp -j REDIRECT --to-port $DNS_PORT
     iptables -t nat -A PREROUTING -p udp --dport 53 -j clash_dns
 fi
 
